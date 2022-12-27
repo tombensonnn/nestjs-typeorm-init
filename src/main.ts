df229c3d85e807d78d5e75e870582449f848b640
= <=>
2022-12-27 19:30:26 +0300

    swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle("nestjs-typeorm").setDescription("A simple API with TypeORM").setVersion("1.0").addTag("typeorm").build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
